f7b1b14983a97fcc039ed528bf238f3a7960bed0
f4tp <mr_treadwell@live.co.uk>
2019-08-19 13:21:05 +0100

    Add project files.

---
+++ VueJsWorkUdemySect3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VueJsWorkUdemySect3.MvcApplication" Language="C#" %>
---
+++ VueJsWorkUdemySect3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VueJsWorkUdemySect3.Startup))]
+namespace VueJsWorkUdemySect3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
